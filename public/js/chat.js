1df61d297dd006de33b5ba197d3d32600489a796
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-28 13:31:57 +0300

    Changed message format

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,60 @@
 const socket = io();
 
 const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const sendLocationButton = document.querySelector('#send-location');
+const messages = document.querySelector('#messages');
+
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const urlTemplate = document.querySelector('#url-template').innerHTML;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    button.setAttribute('disabled', 'disabled');
+
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message, (error) => {
+
+        button.removeAttribute('disabled');
+        input.value = null;
+        input.focus();
+
         if (error) {
             return console.log(error);
         }
 
         console.log('Message delivered');
     });
-    e.target.elements.message.value = null;
 });
 
 socket.on('message', (msg) => {
     console.log(msg);
+    const html = Mustache.render(messageTemplate, {
+        createdAt: moment(msg.createdAt).format('HH:mm'),
+        message: msg.text,
+    });
+    messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+socket.on('locationMessage', (msg) => {
+    const html = Mustache.render(urlTemplate, {
+        url: msg.text,
+        createdAt: moment(msg.createdAt).format('HH:mm'),
+    });
+    messages.insertAdjacentHTML('beforeend', html);
+});
+
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
 
+    sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
+        sendLocationButton.removeAttribute('disabled');
         socket.emit('sendLocation', { latitude: position.coords.latitude, longitude: position.coords.longitude }, (ack) => {
             console.log(ack);
         });
